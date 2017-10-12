5bc697f5d25852bc9f7bda8df7f772ec979e5f34
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-10-12 19:27:29 -0400

    server package fix

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
-
 const app = express();
 
 // Serve static files from the React app
