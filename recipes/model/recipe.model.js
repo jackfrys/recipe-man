b225decd195f04673e13dd8706c303140c4e6f95
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-11-15 10:00:41 -0500

    user model, recipe model

---
+++ recipes/model/recipe.model.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+var recipeSchema = require("./schema/recipe.schema");
+var db = require("../database");
+var recipeModel = mongoose.model("RecipeModel", recipeSchema);
+
+recipeModel.createRecipe = function (userId, recipe) {
+    recipe.user = mongoose.Schema.Types.ObjectId(userId);
+    return recipeModel.create(recipe);
+};
+
+recipeModel.recipesByUser = function (userId) {
+    return recipeModel.find({user:mongoose.Schema.Types.ObjectId(userId)});
+};
+
+recipeModel.sharedWithUser = function (userId) {
+    return recipeModel.find({shared:{$in:mongoose.Schema.Types.ObjectId(userId)}});
+};
+
+recipeModel.updateRecipe = function (recipeId, recipe) {
+    return recipeModel.findByIdAndUpdate(recipeId, {$set:recipe});
+};
+
+recipeModel.deleteRecipe = function (recipeId) {
+    return recipeModel.findByIdAndRemove(recipeId);
+}
+
+module.exports = recipeModel;
