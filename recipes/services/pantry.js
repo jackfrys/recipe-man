792932d1fe91e197208a91a11ac91908e41e6e2c
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-11-15 19:55:42 -0500

    export pantry api

--- recipes/services/pantry.js
+++ recipes/services/pantry.js
@@ -2,3 +2,22 @@ var app = require("../../express");
 var mongoose = require("mongoose");
 var pantryModel = require("../model/pantry.model");
 
+app.get("/api/:uid/pantry", function (req, res) {
+    pantryModel.pantryForUser(req.params.uid).then(function (data) {
+        res.json(data);
+    });
+});
+
+app.post("/api/:uid/pantry/add", function (req, res) {
+    pantryModel.addPantryForUser(req.params.uid, req.body).then(function () {
+        res.send(200);
+    });
+});
+
+app.put("/api/:pid/update", function (req, res) {
+    pantryModel.updatePantry(req.params.pid, req.body).then(function () {
+        res.send(200);
+    });
+});
+
+module.exports = app;
