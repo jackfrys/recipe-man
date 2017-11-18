6e6c731fafd64fd1ef9d43a4992f7313bdf31904
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-11-18 17:52:15 -0500

    changes

--- recipes/model/database.js
+++ recipes/model/database.js
@@ -1,10 +1,7 @@
 var q = require("q");
 var connectionString = 'mongodb://127.0.0.1:27017/recipe-man'; // for local
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds153392.mlab.com:53392/heroku_628hll6f'; // user yours
+if (process.env.MONGODB_URI) { // check if running remotely
+    var connectionString = process.env.MONGODB_URI;
 }
 
 var mongoose = require("mongoose");
