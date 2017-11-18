3fbfa9a9f66bd5c848d100c7be96d127fa0cc739
trentduffy <ct_trentduffy@me.com>
2017-11-18 16:29:01 -0500

    first build

--- express.js
+++ express.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 app.express = express;
+
+app.use(express.static('client/build'))
+
+app.use('/static', express.static('client/build/static'))
+
+
 module.exports = app;
