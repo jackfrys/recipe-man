0380d8018f3ef6648b5c031d3d8c0b4d4a7ed003
trentduffy <ct_trentduffy@me.com>
2017-12-03 23:17:46 -0500

    categories broken in progress

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,23 +1,60 @@
 import React, {Component} from 'react';
 
+import Chip from 'material-ui/Chip';
+
+const styles = {
+  chip: {
+    margin: 4,
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+};
+
 
 class Category extends Component {
 
 
-    // constructor(props) {
-    //     super(props);
-    //
-    //
-    // }
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            "categories": []
+        }
 
+    }
+
+    renderCategory() {
+        return (
+            <Chip
+             style={styles.chip}
+           >
+             Text Chip
+           </Chip>
+        )
+    }
+
+    componentDidMount() {
+        fetch(`https://recipe-man-db.herokuapp.com/api/categories`)
+            .then(results => {
+                return results.json();
+            }).then(data => {
+            let categories = data;
+            console.log(categories)
+            this.setState({
+                "categories": categories
+            });
+        });
+    }
 
     render() {
 
         return (
-            <p>
-                Hi
-            </p>
+            <div>
+            {this.state.categories.map(this.renderIngredient)}
+            </div>
+
 
         );
     }
