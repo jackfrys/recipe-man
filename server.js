16694019673c81858249b3736e28dbf3b298de3e
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-12-10 01:41:13 -0500

    works with react

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var express = app.express;
 app.set('view engine', 'ejs');
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
