551f092cc8d11939d8071b03712bc645a548d3fc
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-12-21 20:21:46 -0500

    renamed services

--- recipes/app.js
+++ recipes/app.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
-    var userService = require("./services/users");
-    var pantryService = require("./services/pantry");
-    var recipeService = require("./services/recipes");
-    var categoryService = require("./services/category");
+    var userService = require("./services/user.service");
+    var pantryService = require("./services/pantry.service");
+    var recipeService = require("./services/recipe.service");
+    var categoryService = require("./services/category.service");
 }
